11615ea130cbd0e88c208ca3a424ac52b1cb6687
Cédric Bonhomme <cedric@cedricbonhomme.org>
2024-11-05 21:44:33 +0100

    chg: [stream] Added detection of vulnerabilities from GSD, CSAF CERT-Bund, CSAF CISCO, and CSAF Hedhat.

--- fedivuln/stream.py
+++ fedivuln/stream.py
@@ -24,7 +24,11 @@ class VulnStreamListener(StreamListener):
         self.vulnerability_pattern = re.compile(
             r"\b(CVE-\d{4}-\d{4,})\b"  # CVE pattern
             r"|\b(GHSA-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4})\b"  # GHSA pattern
-            r"|\b(PYSEC-\d{4}-\d{2,5})\b",  # PYSEC pattern
+            r"|\b(PYSEC-\d{4}-\d{2,5})\b"  # PYSEC pattern
+            r"|\b(GSD-\d{4}-\d{4,5})\b"  # GSD pattern
+            r"|\b(wid-sec-w-\d{4}-\d{4})\b"  # CERT-Bund pattern
+            r"|\b(cisco-sa-\d{8}-[a-zA-Z0-9]+)\b"  # CISCO pattern
+            r"|\b(RHSA-\d{4}:\d{4})\b",  # RedHat pattern
             re.IGNORECASE,
         )
         self.sighting = sighting
